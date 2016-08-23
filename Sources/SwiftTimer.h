253a4e7925ca0989d0ef4a568e749679e20aa117
100mango <100mango@gmail.com>
2016-08-23 12:58:46 +0800

    init

---
+++ Sources/SwiftTimer.h
@@ -0,0 +1,19 @@
+//
+//  SwiftTimer.h
+//  SwiftTimer
+//
+//  Created by mangofang on 16/8/23.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftTimer.
+FOUNDATION_EXPORT double SwiftTimer_VersionNumber;
+
+//! Project version string for SwiftTimer.
+FOUNDATION_EXPORT const unsigned char SwiftTimer_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftTimer_iOS/PublicHeader.h>
+
+
---
+++ Sources/SwiftTimer.swift
@@ -0,0 +1,9 @@
+//
+//  SwiftTimer.swift
+//  SwiftTimer
+//
+//  Created by mangofang on 16/8/23.
+//
+//
+
+import Foundation
